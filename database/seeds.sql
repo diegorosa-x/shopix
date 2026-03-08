72a29e9314a6a1bed535e45e1c88ee9a3409ffb8
Diegodossantos23 <diegodossantos900@outlook.com>
2026-03-08 04:40:26 -0300

    chore(db): add admin seed for profiles table

--- database/seeds.sql
+++
@@ -1,9 +0,0 @@
--- Seed admin user profile
-
-insert into profiles (id, email, name, role)
-select id, email, 'Admin', 'admin'
-from auth.users
-where email = 'admin@shopix.com'
-and not exists (
-  select 1 from profiles where profiles.id = auth.users.id
-);
